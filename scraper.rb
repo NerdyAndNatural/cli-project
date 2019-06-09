be13a357ca9d66102d87b8915788d5a77c53d221
nerdyandnatural <srwilliams8678@gmail.com>
2019-06-09 16:21:30 -0400

    made changes to scraper

--- scraper.rb
+++ scraper.rb
@@ -1,13 +1,22 @@
-require 'Nokogirl'
+require 'nokogiri'
+require 'pry'
 
 class Scraper
-    def initialize
-        @main_url = "http://www.tigerdirect.com/applications/category/category_slc.asp?CatId=4935"
+
+    def open
+        Nokogiri::HTML(open("http://www.tigerdirect.com/applications/category/category_slc.asp?CatId=4935"))    
     end
 
     def info
-        name = parse_page.css(".itemName"){ |name| name.text}.compact
+        laptop = parse_page.css(".itemName"){ |laptop| name.text}.compact
         price = parse_page.css(".oldPrice"){ |price| price.text}.compact
-        
+
+    end
+
+    scraper = Scraper.new
+ 
+    def display
+        puts "Laptop: #{laptop[index]}, Price: #{price[index]}"
     end
 end   
+
