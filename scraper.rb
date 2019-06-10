b3930c1593fe80fad96ab9e7c0d8bea2cf3b9894
nerdyandnatural <srwilliams8678@gmail.com>
2019-06-09 23:35:49 -0400

    change web address

--- scraper.rb
+++ scraper.rb
@@ -6,17 +6,17 @@ class Scraper
 attr_accessor :product, :price, :model
 
  def get_page
-    doc = HTTParty.get("http://www.tigerdirect.com/applications/category/category_tlc.asp?CatId=17")
+    doc = HTTParty.get("https://www.bestbuy.com/site/searchpage.jsp?_dyncharset=UTF-8&browsedCategory=abcat0500000&id=pcat17071&iht=n&ks=960&list=y&qp=category_facet%3Dpcmcat138500050001%5Ecurrentoffers_facet%3DCurrent%20Deals~On%20Sale&sc=Global&st=categoryid%24abcat0500000&type=page&usc=All%20Categories")
     @product ||= Nokogiri::HTML(doc)
   end
     
     def model
-        @title||= get_page.css(".itemName").text
+        @title||= get_page.css(".sku-header").text
 
     end
 
     def cost
-       @price||= get_page.css(".salePrice").text
+       @price||= get_page.css(".priceView-customer-price").text
     end
 
   
