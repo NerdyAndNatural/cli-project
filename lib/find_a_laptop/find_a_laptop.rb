666c580eab638d5aaacba8e43f9097a718799539
nerdyandnatural <srwilliams8678@gmail.com>
2019-06-10 21:35:18 -0400

    update CLI

--- lib/find_a_laptop/find_a_laptop.rb
+++ lib/find_a_laptop/find_a_laptop.rb
@@ -1,25 +1,25 @@
 
+require 'pry'
 
-def findalaptop
-    #def initialize
-       # @setup = Laptop.new
-        #@scraper - Scraper.new
-    #end
 
-    def research
-        puts "Welcome! Let's help you find a laptop"
+class Findalaptop
+
+    def initialize
+        @scraper - Scraper.new
+    end
+
+    def run
+        puts "Thanks for using Find A Laptop"
+        search
+    end
+
+    def search
         puts "What is your price range?"
         puts "A) $100-$500, B)$500-$1000 C)$1000-$2000"
         price = gets.option
         
-        loop do
-            if price include?(A, B, C)
-                puts "Here are your options"
-            else
-                puts "Please select A, B, or C"
-            end
         end
     end
 
+    Findalaptop::CLI.new.run
 
-findalaptop 
